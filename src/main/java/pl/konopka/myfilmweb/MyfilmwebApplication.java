8947d6c76b324fe95cd6db056a5afbe0de30fee9
Marcin Konopka <marcinkonopka123@wp.pl>
2019-06-16 13:15:05 +0200

    Generated project

---
+++ src/main/java/pl/konopka/myfilmweb/MyfilmwebApplication.java
@@ -0,0 +1,13 @@
+package pl.konopka.myfilmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfilmwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfilmwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/konopka/myfilmweb/MyfilmwebApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.konopka.myfilmweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfilmwebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
